d749393ae7cd2c77390297146bccd7d4154f7d83
LeenMass <leen.msarwy12@gmail.com>
2024-08-17 18:55:20 +0300

    updating category

--- finalproj/src/Administration/Categories/Category.jsx
+++ finalproj/src/Administration/Categories/Category.jsx
@@ -1,16 +1,36 @@
-import React from "react";
-import { deleteDoc, doc } from "firebase/firestore";
+import React, { useState } from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import db from "../../firebase";
 const Category = ({ data }) => {
+  const [edit, setEdit] = useState(false);
+  const [updatetitle, setUpdateTitle] = useState({ title: data.title });
   const deleteCategory = async () => {
     await deleteDoc(doc(db, "categories", data.id));
   };
-
+  const updateCategore = async () => {
+    await updateDoc(doc(db, "categories", data.id), updatetitle);
+    setEdit(false);
+  };
   return (
     <div key={data.id} style={{ border: "2px solid black" }}>
-      {data.title}
-      <button>Update</button>
-      <button onClick={deleteCategory}>Remove</button>
+      {edit ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) =>
+              setUpdateTitle({ ...updatetitle, title: e.target.value })
+            }
+            defaultValue={updatetitle.title}
+          />
+          <button onClick={updateCategore}>edit</button>
+        </>
+      ) : (
+        <>
+          {updatetitle.title}
+          <button onClick={() => setEdit(!edit)}>Update</button>
+          <button onClick={deleteCategory}>Remove</button>
+        </>
+      )}
     </div>
   );
 };
