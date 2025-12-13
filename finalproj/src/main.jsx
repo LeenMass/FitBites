bc843995caafbf2a9d39e6e39df4832179dc44da
Leen Massarwy <115391076+LeenMass@users.noreply.github.com>
2025-12-13 21:42:13 +0200

    modified main jsx

--- finalproj/src/main.jsx
+++ finalproj/src/main.jsx
@@ -3,13 +3,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit"; //creating store
+import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./usersSlice.js";
 const store = configureStore({
   reducer: {
     usersD: usersReducer,
   },
-}); // in the object we put all the reducers
+});
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
